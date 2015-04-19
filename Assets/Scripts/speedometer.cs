5618fd2cee6f42afd73242b844b5e1c5a7799bfc
tb11235 <tb11235>
2015-04-19 03:28:18 -0500

    Added speedometer prefab

---
+++ Assets/Scripts/speedometer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speedometer : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3 (0, 0, 3.275F * -player.rigidbody.velocity.magnitude);
+		// x' = xcos(theta) - ysin(theta)
+		// y' = xsin(theta) - ycos(theta)
+	}
+}

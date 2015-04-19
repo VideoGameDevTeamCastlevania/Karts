81f1ee2068633689c54ef8a13a260e9169f9e8d0
tb11235 <tb11235>
2015-04-19 00:56:43 -0500

    Added basic engine sound

---
+++ Assets/Scripts/carSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carSound : MonoBehaviour {
+	
+	public AudioSource audio_engine;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		audio_engine.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		speed = rigidbody.velocity.magnitude;
+		audio_engine.pitch = Mathf.Clamp (Mathf.Log10 (rigidbody.velocity.magnitude), 0.5F, 3);
+	}
+}

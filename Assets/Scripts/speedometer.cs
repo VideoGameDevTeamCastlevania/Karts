a31cadce5b78680b5752e2cb300b60b6ffc4253c
tb11235 <tjb.programming@gmail.com>
2015-04-19 04:36:24 -0500

    Changed speedometer setup

--- Assets/Scripts/speedometer.cs
+++ Assets/Scripts/speedometer.cs
@@ -3,17 +3,16 @@ using System.Collections;
 
 public class speedometer : MonoBehaviour {
 
-	private GameObject player;
+	public GameObject vehicle;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.eulerAngles = new Vector3 (0, 0, 3.275F * -player.rigidbody.velocity.magnitude);
-		// x' = xcos(theta) - ysin(theta)
-		// y' = xsin(theta) - ycos(theta)
+		//move the needle as speed changes
+		transform.eulerAngles = new Vector3 (0, 0, 3.275F * -vehicle.rigidbody.velocity.magnitude); 
 	}
 }

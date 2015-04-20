e64c3ab990342c71b2257b5ad17d5b0a068b8dba
ConcreteBarricade <tcdinh@live.com>
2015-04-19 19:03:36 -0500

    Basic AI
    Here's some basic AI that I've worked on. I may have added too many
    waypoints. The agent does clip through the ground though.

---
+++ Assets/Scripts/RaceAgent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceAgent : MonoBehaviour {
+	public GameObject targetWaypoint;
+	public Vector3 targetPosition;
+	public GameObject WaypointObj;
+	public int distranceFromWaypoint;
+	private int waypointIndex  = 0;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.angularSpeed = 360;
+		WaypointObj.GetComponent<WaypointManager> ();
+		targetWaypoint = WaypointObj.GetComponent<WaypointManager> ().Waypoints [waypointIndex];
+		targetPosition = targetWaypoint.transform.position;
+		agent.SetDestination (targetPosition);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Vector3.Distance(transform.position, targetPosition) < distranceFromWaypoint)) { //& (targetWaypoint.GetComponent<Waypoint>().end == false)) {
+			waypointIndex += 1;
+			targetWaypoint = WaypointObj.GetComponent<WaypointManager> ().Waypoints [waypointIndex];
+			targetPosition = targetWaypoint.transform.position;
+			agent.SetDestination (targetPosition);
+		}
+	}
+}
---
+++ Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointManager : MonoBehaviour {
+	public GameObject[] Waypoints;
+	public GameObject raceAgent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

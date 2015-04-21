0dae65c1aa2a2a664ecc048e216cb5514e046872
Alianny <alianny@okstate.edu>
2015-04-21 17:16:49 -0500

    ADDED CAR TO DESERT MAP

---
+++ Assets/CARS/SpinWheel.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinWheel : MonoBehaviour {
+
+	public WheelCollider RL, RR, FL, FR;
+	public Transform RRL, RRR, FFL, FFR, HEAD, LARM, RARM;
+	private Rigidbody RB;
+	public int toe, toe2, toe3;
+	private float DontWaste;
+	// Use this for initialization
+	void Start () {
+		RB = GetComponent<Rigidbody> ();
+		Vector3 y = RB.centerOfMass;
+		y.y = -1.3f;
+		RB.centerOfMass = y;
+		DontWaste = FL.steerAngle;
+	}
+	void Update(){
+
+		FFL.Rotate (0,0,FL.rpm / 60 * -360 * Time.deltaTime);
+		FFR.Rotate (0,0,FR.rpm / 60 * 360 * Time.deltaTime); 
+		if(FL.steerAngle != DontWaste) {
+						Vector3 turnL = FFL.localEulerAngles;
+						turnL.y = FL.steerAngle - 90; 
+						FFL.localEulerAngles = turnL;
+						turnL.y = FL.steerAngle + 90; 
+						FFR.localEulerAngles = turnL;
+
+						turnL.z = FL.steerAngle + 2;
+						turnL.x = -FL.steerAngle + 2;
+						turnL.y = FL.steerAngle;
+						HEAD.localEulerAngles = turnL;
+						DontWaste = FL.steerAngle;
+						print (Time.deltaTime);
+				}
+			//WheelHit slide;
+			//FR.GetGroundHit( slide );
+			
+			// if the slip of the tire is greater than 2.0, and the slip prefab exists, create an instance of it on the ground at
+			// a zero rotation.
+			//if ( Mathf.Abs( slide.sidewaysSlip ) > .8 )
+			//{
+				//skid.gameObject.active = true;
+			//}
+			//else if ( Mathf.Abs( slide.sidewaysSlip ) <= .75 )
+			//{
+				//skid.gameObject.active = false;
+			//}
+}
+
+
+
+	// Update is called once per frame
+void FixedUpdate () {
+
+		//print ("V:" + RB.velocity.magnitude);
+		if (RB.velocity.magnitude > 1) {
+						RR.motorTorque = (50 - (.8f * RB.velocity.magnitude)) * Input.GetAxis ("Vertical");
+						RL.motorTorque = (50 - (.8f * RB.velocity.magnitude)) * Input.GetAxis ("Vertical");
+				} else {
+			RR.motorTorque = 100 *Input.GetAxis ("Vertical");
+			RL.motorTorque = 100 * Input.GetAxis ("Vertical");
+		}	
+				
+
+		if (RB.velocity.magnitude > 1) {
+						FL.steerAngle = (10 - (.1f * RB.velocity.magnitude)) * Input.GetAxis ("Horizontal");
+						FR.steerAngle = (10 - (.1f * RB.velocity.magnitude)) * Input.GetAxis ("Horizontal");
+				} else {
+			FL.steerAngle = 10 * Input.GetAxis ("Horizontal");
+			FR.steerAngle = 10 * Input.GetAxis ("Horizontal");		
+			}
+		}
+
+		
+	}
+

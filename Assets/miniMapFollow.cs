1584e96572c9fec128d57337537c3ecb9d7476ed
Jim Stack <jpstack@gmail.com>
2015-04-21 23:18:19 -0500

    Added mini map to the 3 tracks. An icon in the mini map camera would be more helpful, but it is a start.

---
+++ Assets/miniMapFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class miniMapFollow : MonoBehaviour {
+
+    public Transform kart;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Transform trans = GetComponent<Transform>();
+
+        trans.position = new Vector3(kart.position.x, trans.position.y, kart.position.z); 
+	}
+}

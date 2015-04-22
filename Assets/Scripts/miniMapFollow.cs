de51f7eda020e7999caa14d8130b0ac78bc1902f
tb11235 <tjb.programming@gmail.com>
2015-04-22 04:49:44 -0500

    Added player tag to player vehicles; changed how the minimap was finding the player.

---
+++ Assets/Scripts/miniMapFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class miniMapFollow : MonoBehaviour {
+
+    private GameObject kart;
+	private Transform indicator;
+
+	public float indicator_distance; //from camera
+
+	// Use this for initialization
+	void Start () 
+    {
+		kart = GameObject.FindWithTag ("Player");
+		indicator = transform.Find ("PlayerIndicator");	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Transform trans = GetComponent<Transform>();
+
+		// set up camera position
+        trans.position = new Vector3(kart.transform.position.x, trans.position.y, kart.transform.position.z); 
+		// position indicator on minimap
+		indicator.position = new Vector3 (kart.transform.position.x, trans.position.y - indicator_distance, kart.transform.position.z);
+
+	}
+}

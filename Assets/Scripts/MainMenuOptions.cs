1effb46b9f56ea962b4951c8467f5d3b92df8ce6
tb11235 <tjb.programming@gmail.com>
2015-04-24 00:55:39 -0500

    Stitching menus together.

---
+++ Assets/Scripts/MainMenuOptions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void load_menu (string menu) {
+		Application.LoadLevel (menu);
+	}
+
+	public void quit () {
+		Application.Quit ();
+	}
+}

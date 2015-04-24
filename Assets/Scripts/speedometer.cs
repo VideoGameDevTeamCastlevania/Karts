0ff239ea7c4050b7c1911ffbd666fb04d4da71f3
tb11235 <tjb.programming@gmail.com>
2015-04-24 03:17:16 -0500

    Anchored speedometer to edge of screen; changed how the speedometer was assigned to the player.

--- Assets/Scripts/speedometer.cs
+++ Assets/Scripts/speedometer.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class speedometer : MonoBehaviour {
 
-	public GameObject vehicle;
+	private GameObject vehicle;
+	private float calibration; //calibrate the needle to rotate accurately
 
 	// Use this for initialization
 	void Start () {
-	
+		vehicle = GameObject.FindGameObjectWithTag ("Player");
+		calibration = 3.275F; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//move the needle as speed changes
-		transform.eulerAngles = new Vector3 (0, 0, 3.275F * -vehicle.rigidbody.velocity.magnitude); 
+		transform.eulerAngles = new Vector3 (0, 0, calibration * -vehicle.rigidbody.velocity.magnitude); 
 	}
 }

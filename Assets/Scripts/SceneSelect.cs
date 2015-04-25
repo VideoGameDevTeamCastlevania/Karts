3f7c1eb5da9dd8a216f8c462c0e858253e75bb8e
tb11235 <tjb.programming@gmail.com>
2015-04-24 21:40:33 -0500

    Changed pause menu, added tutorial option in titlescreen.

--- Assets/Scripts/SceneSelect.cs
+++ Assets/Scripts/SceneSelect.cs
@@ -5,12 +5,9 @@ public class SceneSelect : MonoBehaviour {
 
     public Texture2D boxbg;
 
-    public string SceneOne;
-    private string SceneOneLable = "Mountains";
-    public string SceneTwo;
-    private string SceneTwoLable = "Desert";
-    public string SceneThree;
-    private string SceneThreeLable = "Rolling Hills";
+    private string main_menu_label = "Main Menu";
+    private string options_label = "Options";
+	private string restart_label = "Restart";
 
     private bool showMenu;
 
@@ -19,33 +16,35 @@ public class SceneSelect : MonoBehaviour {
         showMenu = false;
     }
 
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (!showMenu)
+				showMenu = true;
+			else
+				showMenu = false;
+		}
+	}
+
 	// Use this for initialization
 	void OnGUI () 
-    {
-        if (Input.GetKey(KeyCode.M)) showMenu = true;
-        else if (Input.GetKey(KeyCode.Escape)) showMenu = false;
-
-        if (showMenu)
-        {
-            GUI.BeginGroup(new Rect(Screen.width / 2f - 50f, Screen.height / 2f - 50f, Screen.width, Screen.height));
-            GUI.Box(new Rect(0, 0, 160, 200), boxbg);
-            if (GUI.Button(new Rect(10, 10, 125, 30), SceneOneLable))
-            {
-                Application.LoadLevel(SceneOne);
-            }
-            if (GUI.Button(new Rect(10, 50, 125, 30), SceneTwoLable))
-            {
-                Application.LoadLevel(SceneTwo);
-            }
-            if (GUI.Button(new Rect(10, 90, 125, 30), SceneThreeLable))
-            {
-                Application.LoadLevel(SceneThree);
-            }
-            if (GUI.Button(new Rect(10, 130, 125, 30), "Quit"))
-            {
-                Application.Quit();
-            }
-            GUI.EndGroup();
-        }
+    {   
+		if (showMenu) {
+			GUI.BeginGroup (new Rect (Screen.width / 2f - 50f, Screen.height / 2f - 50f, Screen.width, Screen.height));
+			GUI.Box (new Rect (0, 0, 160, 200), boxbg);
+			if (GUI.Button (new Rect (10, 10, 125, 30), main_menu_label)) {
+				Application.LoadLevel ("TitleScreen");
+			}
+			if (GUI.Button (new Rect (10, 50, 125, 30), options_label)) {
+//                Application.LoadLevel("Options");
+			}
+			if (GUI.Button (new Rect (10, 90, 125, 30), restart_label)) {
+				Application.LoadLevel (Application.loadedLevelName);
+			}
+			if (GUI.Button (new Rect (10, 130, 125, 30), "Quit")) {
+				Application.Quit ();
+			}
+			GUI.EndGroup ();
+		} 
     }
 }

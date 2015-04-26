479f50bac7bc487645d6d6f9c2934faae222a363
tb11235 <tjb.programming@gmail.com>
2015-04-26 01:04:27 -0500

    Small fixes to the pause menu.

--- Assets/Scripts/SceneSelect.cs
+++ Assets/Scripts/SceneSelect.cs
@@ -38,17 +38,20 @@ public class SceneSelect : MonoBehaviour {
 		if (showMenu) {
 			GUI.BeginGroup (new Rect (Screen.width / 2f - 50f, Screen.height / 2f - 50f, Screen.width, Screen.height));
 			GUI.Box (new Rect (0, 0, 160, 200), boxbg);
-			if (GUI.Button (new Rect (10, 10, 125, 30), main_menu_label)) {
-				Application.LoadLevel ("TitleScreen");
+			if (GUI.Button (new Rect (10, 10, 125, 30), "Resume")) {
+				Time.timeScale = 1.0F;
+				showMenu = false;
 			}
 			if (GUI.Button (new Rect (10, 50, 125, 30), options_label)) {
-//                Application.LoadLevel("Options");
+//                Spawn Options menu here
 			}
 			if (GUI.Button (new Rect (10, 90, 125, 30), restart_label)) {
+				Time.timeScale = 1.0F;
 				Application.LoadLevel (Application.loadedLevelName);
 			}
 			if (GUI.Button (new Rect (10, 130, 125, 30), "Quit")) {
-				Application.Quit ();
+				Time.timeScale = 1.0F;
+				Application.LoadLevel("TitleScreen");
 			}
 			GUI.EndGroup ();
 		} 

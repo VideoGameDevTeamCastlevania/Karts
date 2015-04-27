412752a759ce2ab636c2ebc10641359170f31a99
ConcreteBarricade <tcdinh@live.com>
2015-04-26 21:20:30 -0500

    Update to Char Spawn and Map Select
    Had to move character prefabs to a "resources" folder in order to
    instantiate them using Resources.Load

--- Assets/Scripts/CharMapSpawnThing.cs
+++ Assets/Scripts/CharMapSpawnThing.cs
@@ -28,4 +28,34 @@ public class CharMapSpawnThing : MonoBehaviour {
 	public void loadMountains(){
 	}
 
+	public void spawn(string character) {
+		GameObject playerSpawn = GameObject.FindGameObjectWithTag ("PlayerSpawn");
+		GameObject[] AISpawn = GameObject.FindGameObjectsWithTag ("AISpawn");
+		GameObject player;
+		GameObject AI1;
+		GameObject AI2;
+		if (character == "Girl") {
+			player = Instantiate (Resources.Load("GirlScout"), playerSpawn.transform.position, playerSpawn.transform.rotation) as GameObject;
+			AI1 = Instantiate(Resources.Load("Dog"), AISpawn[1].transform.position, AISpawn[1].transform.rotation) as GameObject;
+			AI1.tag = "AI";
+			AI2 = Instantiate(Resources.Load("Marsian"), AISpawn[2].transform.position, AISpawn[2].transform.rotation) as GameObject;
+			AI2.tag = "AI";
+		}
+		if (character == "Dog") {
+			player = Instantiate (Resources.Load("Dog"), playerSpawn.transform.position, playerSpawn.transform.rotation) as GameObject;
+			AI1 = Instantiate(Resources.Load("GirlScout"), AISpawn[1].transform.position, AISpawn[1].transform.rotation) as GameObject;
+			AI1.tag = "AI";
+			AI2 = Instantiate(Resources.Load("Marsian"), AISpawn[2].transform.position, AISpawn[2].transform.rotation) as GameObject;
+			AI2.tag = "AI";
+		}
+		if (character == "Marsian") {
+			player = Instantiate (Resources.Load("Marsian"), playerSpawn.transform.position,  playerSpawn.transform.rotation)  as GameObject;
+			AI1 = Instantiate(Resources.Load("GirlScout"), AISpawn[1].transform.position, AISpawn[1].transform.rotation) as GameObject;
+			AI1.tag = "AI";
+			AI2 = Instantiate(Resources.Load("Dog"), AISpawn[2].transform.position, AISpawn[2].transform.rotation) as GameObject;
+			AI2.tag = "AI";
+		}
+
+	}
+
 }

d5321d7472d3b5e90f642f0059d63b296b0b247e
tb11235 <tjb.programming@gmail.com>
2015-04-27 18:48:01 -0500

    Spawn options menu from pause menu; Fix Map Select Back Button

--- Assets/Scripts/SceneSelect.cs
+++ Assets/Scripts/SceneSelect.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SceneSelect : MonoBehaviour {
 
     public Texture2D boxbg;
+	public GameObject options_menu;
 
     private string main_menu_label = "Main Menu";
     private string options_label = "Options";
@@ -22,8 +23,6 @@ public class SceneSelect : MonoBehaviour {
 			if (!showMenu) {
 				showMenu = true;
 				Time.timeScale = 0.0F;
-				//TODO: disable sound
-				//TODO: decrease music volume
 			}
 			else {
 				showMenu = false;
@@ -43,7 +42,7 @@ public class SceneSelect : MonoBehaviour {
 				showMenu = false;
 			}
 			if (GUI.Button (new Rect (10, 50, 125, 30), options_label)) {
-//                Spawn Options menu here
+				options_menu.SetActive(true);
 			}
 			if (GUI.Button (new Rect (10, 90, 125, 30), restart_label)) {
 				Time.timeScale = 1.0F;

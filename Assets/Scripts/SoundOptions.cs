95fea67d3d4324e0ebac8c64a8b929b03cd60314
tb11235 <tjb.programming@gmail.com>
2015-04-26 22:13:12 -0500

    More sound options stuff.

--- Assets/Scripts/SoundOptions.cs
+++ Assets/Scripts/SoundOptions.cs
@@ -6,17 +6,23 @@ public class SoundOptions : MonoBehaviour {
 	private float music_volume, sound_volume;
 	
 	private AudioSource music_source;
-	
+	private GameObject main_camera;
+
+	private int level_index;
+
 	// Use this for initialization
 	void Start () {
 		music_volume = 1.0F;
 		sound_volume = 1.0F;
-		music_source = GetComponent<AudioSource> ();
-		music_source.ignoreListenerVolume = true;
+		level_index = Application.loadedLevel;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Application.loadedLevel != level_index) {
+			level_index = Application.loadedLevel;
+			setup_music();
+		}
 		AudioListener.volume = sound_volume;
 		music_source.volume = music_volume;
 	}
@@ -29,5 +35,11 @@ public class SoundOptions : MonoBehaviour {
 	public void setSoundVolume (float volume)
 	{
 		sound_volume = volume;
+	}
+
+	void setup_music () {
+		main_camera = GameObject.FindWithTag ("MainCamera");
+		music_source = main_camera.GetComponent<AudioSource> ();
+		music_source.ignoreListenerVolume = true;
 	}
 }

ae1e913161ddb3d576d006c02131c0d6a2b32b91
ConcreteBarricade <tcdinh@live.com>
2015-04-27 15:16:57 -0500

    Slight update  to AboutScreen
    Just added some small text to About

--- Assets/Scripts/CharMapSpawnThing.cs
+++ Assets/Scripts/CharMapSpawnThing.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class CharMapSpawnThing : MonoBehaviour {
 	private static string charSelection;
 	private static int mapSelectNumber;
-	private int level_index;
 
 	// Use this for initialization
 	void Start () {
@@ -55,7 +54,6 @@ public class CharMapSpawnThing : MonoBehaviour {
 		GameObject player;
 		GameObject AI1;
 		GameObject AI2;
-		print(playerSpawn.transform.position.x.ToString());
 		if (character == "Girl") {
 			player = Instantiate (Resources.Load("GirlScout"), playerSpawn.transform.position, playerSpawn.transform.rotation) as GameObject;
 			AI1 = Instantiate(Resources.Load("DogAI"), AISpawn[0].transform.position, AISpawn[0].transform.rotation) as GameObject;
@@ -65,7 +63,6 @@ public class CharMapSpawnThing : MonoBehaviour {
 		}
 		if (character == "Dog") {
 			player = Instantiate (Resources.Load("Dog"), playerSpawn.transform.position, playerSpawn.transform.rotation) as GameObject;
-			print ("SOmething should be here");
 			AI1 = Instantiate(Resources.Load("GirlScoutAI"), AISpawn[0].transform.position, AISpawn[0].transform.rotation) as GameObject;
 			AI1.tag = "AI";
 			AI2 = Instantiate(Resources.Load("MarsianAI"), AISpawn[1].transform.position, AISpawn[1].transform.rotation) as GameObject;
@@ -77,8 +74,6 @@ public class CharMapSpawnThing : MonoBehaviour {
 			AI1.tag = "AI";
 			AI2 = Instantiate (Resources.Load ("DogAI"), AISpawn [1].transform.position, AISpawn [1].transform.rotation) as GameObject;
 			AI2.tag = "AI";
-		} else {
-			print ("Character is = " + character);
 		}
 	}
 }
